33219108bf5e82189d488e6f1c46cf1eeab62109
iblin0212 <146217791+iblin0212@users.noreply.github.com>
2025-03-10 13:16:39 -0500

    summary_stats

--- cleaning2.R
+++ cleaning2.R
@@ -1,4 +1,4 @@
-## -------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 library(dplyr)
 library(tidyr)
 library(haven)
@@ -7,93 +7,117 @@ library(knitr)
 library(kableExtra)
 library(caret)
 library(stats)
+library(fastDummies)
 
 
-## -------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 cex <- read_dta("~/Desktop/metrics_and_ml/ecma31330_final/113270/cexall.dta")
 psid <- read_dta("~/Desktop/metrics_and_ml/ecma31330_final/113270/data.dta")
 
 
-## -------------------------------------------------------------------------------------------------------
-psid <- dummy_cols(psid, select_columns = c("race", "state", "smsa"))
-
-# Remove columns that represent >2 categorical variables
-psid <- psid %>%
-  select(-c(vet, disab, outkid, unioni, unionj, race, state, smsa))
-
-
-## -------------------------------------------------------------------------------------------------------
-cex <- dummy_cols(cex, select_columns = c("race", "region", "famtype"))
-
-# Remove columns that represent >2 categorical variables
-cex <- cex %>%
-  select(-c(race, region, famtype))
-head(cex)
-
-
-## -------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 natpr <- read_dta("~/Desktop/metrics_and_ml/ecma31330_final/113270/natpr.dta")
 tax9192 <- read_dta("~/Desktop/metrics_and_ml/ecma31330_final/113270/tax9192.dta")
 finprc <- read_dta("~/Desktop/metrics_and_ml/ecma31330_final/113270/finprice.dta")
 
 
-## -------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 psid <- merge(psid, tax9192, by = c("id", "year"))
 psid$ftax[psid$year %in% c(1991, 1992)] <- psid$fiitax[psid$year %in% c(1991, 1992)]
 psid <- psid[, !(names(psid) %in% c("fiitax"))]
 
 
-## -------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 psid <- merge(psid, natpr, by = "year")
 
 
-## -------------------------------------------------------------------------------------------------------
-cex <- cex %>% select(-c(complete, cons1, cons2, family))
-
-
-## -------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 cex <- cex %>%
   mutate(hours = weekh * hourh)
 cex <- cex %>%
   mutate(hourw = weekw * hourw)
 
 
-## -------------------------------------------------------------------------------------------------------
-# for PSID 
+## -----------------------------------------------------------------------------
+cex<- cex %>%
+  mutate(
+    ftax = income - income_at,  # Federal tax estimate
+    asset = pf + pfaw           # Total asset estimate
+  )
+
+
+## -----------------------------------------------------------------------------
+state_to_region <- function(state_code) {
+  if (state_code %in% c(9, 23, 25, 33, 34, 36, 42, 44, 50)) {
+    return(1)  # Northeast
+  } else if (state_code %in% c(17, 18, 19, 20, 26, 27, 29, 31, 38, 39, 46, 55)) {
+    return(2)  # Midwest
+  } else if (state_code %in% c(1, 5, 10, 11, 12, 13, 21, 22, 24, 28, 37, 40, 45, 47, 48, 51, 54)) {
+    return(3)  # South
+  } else if (state_code %in% c(2, 4, 6, 8, 15, 16, 30, 32, 35, 41, 49, 53, 56)) {
+    return(4)  # West
+  } else {
+    return(NA)  # Unknown state code
+  }
+}
+
 psid <- psid %>%
-  mutate(total_income = tyhw + tyoth)
+  mutate(region = sapply(state, state_to_region))
+
+head(psid)
 
 
-## -------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 psid <- psid %>%
-  mutate(hw_laborinc = ly + wly)
+  mutate(race = case_when(
+    race == 1 ~ 1,  # White
+    race == 2 ~ 2,  # Black or African American
+    race == 3 ~ 3,  # Native American
+    race == 4 ~ 4,  # Asian
+    race == 5 ~ NA, # Latino origin
+    race == 6 ~ NA, # Other color
+    race == 7 ~ NA, # Other
+    race == 9 ~ NA, # Don't know
+    TRUE ~ NA      
+  ))
 
 
-## -------------------------------------------------------------------------------------------------------
-cex <- cex %>%
-  mutate(hw_wages = wageh + wagew)
-
-
-## -------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 psid <- psid %>%
-  mutate(hwage_inc = wages * hours)
+  mutate(income = tyhw + tyoth)
 
 
-## -------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
+cex <- cex %>% select(-c(complete, cons1, cons2, cpi, educw, family, famtype, income, income_net, pf, pfaw, pfin, pfood, pfout, wageh, wagew, weekh, weekw, yb))
+
+
+## -----------------------------------------------------------------------------
 cex <- cex %>%
   rename(fsize = ncomp,
          kids = kid,
          educ = educh,
-         total_income = income_at,
+         income = income_at,
          )
 
 
-## -------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 cex <- cex %>%
   mutate(across(everything(), ~ as.numeric(as.character(.))))
 
 
-## -------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 cex <- cex %>%
-  mutate(across(c(hourh, hourw, wageh, wagew, hours, hw_wages), ~ ifelse(is.na(.), 0, .)))
+  mutate(across(c(hours, hourw), ~ ifelse(is.na(.), 0, .)))
+
+
+## -----------------------------------------------------------------------------
+#psid <- dummy_cols(psid, select_columns = c("race", "region"))
+#psid <- psid %>%
+ # select(-c(race, region, state))
+
+
+## -----------------------------------------------------------------------------
+#cex <- dummy_cols(cex, select_columns = c("race", "region"))
+#cex <- cex %>%
+ # select(-c(race, region))
 
