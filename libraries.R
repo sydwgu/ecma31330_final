8eb307997cc521d4b1c37204d0fdce864ece20b8
sydwgu <59843075+sydwgu@users.noreply.github.com>
2025-03-16 21:40:52 -0400

    created library citation .r file

---
+++ libraries.R
@@ -0,0 +1,18 @@
+# install and load the following libraries prior 
+# to running any of the paper's .Rmd or .R files
+
+library(dplyr)
+library(tidyr)
+library(haven)
+library(glmnet)
+library(data.table)
+library(knitr)
+library(kableExtra)
+library(caret)
+library(stats)
+library(gbm)
+library(keras)
+library(nnet)
+library(ggplot2)
+library(webshot2)
+library(stringr)
